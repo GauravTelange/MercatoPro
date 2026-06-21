91648b41129107bed425cfa50d794f9ef3d4b190
Gaurav Telange <gauravtelange4321@gmail.com>
2026-06-22 00:07:40 +0530

    Added Cart Functionality

---
+++ MercatoPro/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using MercatoPro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercatoPro.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(int productId)
+        {
+            int? userId = HttpContext.Session.GetInt32("UserId");
+
+            if (userId == null) {
+
+                return RedirectToAction("Login", "Account");
+            }
+
+            var cartItem = new Cart
+            {
+                UserId = userId.Value,
+                ProductId = productId,
+                Quantity = 1
+            };
+
+            _context.Carts.Add(cartItem);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index","Product");
+        }
+    }
+}
